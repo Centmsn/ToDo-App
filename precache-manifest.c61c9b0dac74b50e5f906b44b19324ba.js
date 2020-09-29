fb414891afee283b1be4adea244f078fc2adc5c8
Wojtek Rygorowicz <wojciech.rygorowicz@gmail.com>
2020-09-29 15:41:16 +0200

    Updates

---
+++ precache-manifest.c61c9b0dac74b50e5f906b44b19324ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aff4564e840eef09b02e1cbe051e406",
+    "url": "/ToDo-App/index.html"
+  },
+  {
+    "revision": "3ee5a54df80fdc977df4",
+    "url": "/ToDo-App/static/css/main.07c69f43.chunk.css"
+  },
+  {
+    "revision": "b638d5e4891abbc26bb6",
+    "url": "/ToDo-App/static/js/2.953a044f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDo-App/static/js/2.953a044f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee5a54df80fdc977df4",
+    "url": "/ToDo-App/static/js/main.856db9a9.chunk.js"
+  },
+  {
+    "revision": "15cef6e4ff235c57cb05",
+    "url": "/ToDo-App/static/js/runtime-main.598e824b.js"
+  }
+]);
